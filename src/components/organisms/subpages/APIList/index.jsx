32f5045743e3ff8429e2153f9e0a4754acd76906
Aichnerc <contact@aichner-christian.com>
2019-10-22 03:57:19 +0200

    Add demo data
    Demo data with Breezometer has been used.

--- src/components/organisms/subpages/APIList/index.jsx
+++ src/components/organisms/subpages/APIList/index.jsx
@@ -6,16 +6,71 @@ import React from 'react';
 // "Material Design for Bootstrap" is a great UI design framework
 import {
   MDBContainer,
+  MDBRow,
+  MDBCol,
+  MDBCard,
+  MDBCardBody,
 } from 'mdbreact';
 
 //> Images
-// Too be added
+// https://breezometer.com
+import breezometerIcon from '../../../../assets/apis/breezometer.jpg';
+
+//> Dummy data
+const data = [
+  {
+    name: "Air Quality",
+    company: "Breezometer",
+    logo: breezometerIcon,
+    categories: [
+      "Environmental",
+      "Air Quality"
+    ],
+    price: undefined,
+    rating: 4,
+    supported: true,
+  }
+]
 
 class APIList extends React.Component {
 
   render() {
     return (
-      <p>API List</p>
+      <div className="p-md-5">
+      <h4>Welcome to the API Explorer</h4>
+      <p>The API Explorer contains all APIs you will be able to use over our service.</p>
+      <hr/>
+      <MDBRow>
+      {data.map((api, key) => {
+        return(
+          <MDBCol md="2" key={key}>
+            <MDBCard>
+              <MDBCardBody>
+                <MDBRow>
+                  <MDBCol className="flex-center">
+                    <div className="p-2">
+                      <p className="lead mb-0">{api.name}</p>
+                      <small className="text-muted d-block">
+                      {api.company}
+                      </small>
+                    </div>
+                    <div className="ml-auto p-2 mb-auto">
+                      <img 
+                      src={api.logo}
+                      width="50px"
+                      alt={api.name}
+                      className="img-fluid align-self-center mr-0"
+                      />
+                    </div>
+                  </MDBCol>
+                </MDBRow>
+              </MDBCardBody>
+            </MDBCard>
+          </MDBCol>
+        );
+      })}
+      </MDBRow>
+      </div>
     );
   }
 }
