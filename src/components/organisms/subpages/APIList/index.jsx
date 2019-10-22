1d0dc63a5f8593521a70774a22a65333a620f958
Aichnerc <contact@aichner-christian.com>
2019-10-22 02:17:01 +0200

    Add subpages
    An APIList subpage has been created.

---
+++ src/components/organisms/subpages/APIList/index.jsx
@@ -0,0 +1,28 @@
+//> React
+// Contains all the functionality necessary to define React components
+import React from 'react';
+
+//> MDB
+// "Material Design for Bootstrap" is a great UI design framework
+import {
+  MDBContainer,
+} from 'mdbreact';
+
+//> Images
+// Too be added
+
+class APIList extends React.Component {
+
+  render() {
+    return (
+      <p>API List</p>
+    );
+  }
+}
+
+export default APIList;
+
+/** 
+ * SPDX-License-Identifier: (EUPL-1.2)
+ * Copyright © 2019 Werbeagentur Christian Aichner
+ */
---
+++ src/components/organisms/subpages/index.js
@@ -0,0 +1,12 @@
+//> Components
+// Import all components to export them for easy access from parent components
+import APIList from './APIList';
+
+export {
+    APIList,
+};
+
+/** 
+ * SPDX-License-Identifier: (EUPL-1.2)
+ * Copyright © 2019 Werbeagentur Christian Aichner
+ */
